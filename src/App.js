4350cdcfe71a6965e3ec7835ef3a69b7178d4951
sandeep <beingsandeepg@gmail.com>
2023-04-21 11:47:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {Route,Routes,Router} from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
